f12316d9373897c15279f76c19641db1bcfce228
EkaterinaEsina <eesina@131.ru>
2024-10-21 23:59:10 +0500

    fix: рефакторинг

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -10,7 +10,7 @@ export const Header = (props) => {
 
         <ul id="nav-mobile" className="right hide-on-med-and-down">
           <li>
-            <Link to='/shop'>Meals</Link>
+            <Link to='/meals'>Meals</Link>
           </li>
           <li>
             <Link to='/shop'>Shop</Link>
